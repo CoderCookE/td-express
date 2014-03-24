ad0381310ec6e6b893d0af515c08b3056875b1f8
James Drury <jdrury86@gmail.com>
2014-03-24 16:20:59 -0400

    adds templating, basic views, basic form

--- app.js
+++ app.js
@@ -4,17 +4,20 @@
  */
 
 var express = require('express');
+var swig = require('swig');
 var routes = require('./routes');
-var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 
 var app = express();
+app.engine('html', swig.renderFile);
 
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'html');
+
+// express middleware
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
@@ -26,10 +29,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
+  swig.setDefaults({ cache: false });
 }
 
 app.get('/', routes.index);
-app.get('/users', user.list);
+app.get('/test', routes.create);
+app.post('/run_test', routes.run_test);
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,11 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
 };
+
+exports.create = function(req, res){
+  res.render('new_test');
+};
+
+exports.run_test = function(req, res){
+  res.send(200);
+}
