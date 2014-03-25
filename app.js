44b5db7828a18af977299692864727d76e384228
Lauren Ashpole <lauren@laurenashpole.com>
2014-03-25 12:56:06 -0400

    wired up height and width inputs to screenshot options

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var swig    = require('swig');
 var routes  = require('./routes');
 var http    = require('http');
 var path    = require('path');
+var AWS     = require('aws-sdk');
 
 var app = express();
 app.engine('html', swig.renderFile);
@@ -24,15 +25,22 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
 if ('development' == app.get('env')) {
+  local_config = require('./local_config');
+  AWS.config.update({accessKeyId: local_config.AMAZON_ACCESS_ID, secretAccessKey: local_config.AMAZON_SECRET_KEY});
+
   app.use(express.errorHandler());
   swig.setDefaults({ cache: false });
+} else {
+	local_config = {
+		AMAZON_ACCESS_ID: process.env.AMAZON_ACCESS_ID,
+		AMAZON_SECRET_KEY: process.env.AMAZON_SECRET_KEY
+	};
 }
 
 // routes ===========
 app.get('/', routes.index);
 app.get('/test', routes.create);
 app.post('/run_test', routes.run_test);
-
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
